e21bdfddc0159378bb1375d10b5514bbe6df2004
Marco Antônio <markomasf@gmail.com>
2023-03-08 11:04:07 -0300

    correções funções

--- banco.php
+++ banco.php
@@ -1,30 +1,26 @@
 <?php
 
-include 'funcoes.php';
+require 'funcoes.php';
 
 $contas = [
     '123.456.789-10' => [
         'titular' => 'Marco',
         'valor' => 100000
-    ], 
+    ],
     '123.456.789-11' => [
         'titular' => 'Lorene',
         'valor' => 200000
-    ], 
+    ],
     '123.456.789-12' => [
         'titular' => 'José',
         'valor' => 500
     ]
 ];
 
-
-
 sacar($contas['123.456.789-12']['valor'], 326);
 
 depositar($contas['123.456.789-10']['valor'], 600);
 
-
-foreach($contas as $cpf => $conta){
+foreach ($contas as $cpf => $conta) {
     quebrarLinha("$cpf =  {$conta['titular']}  =  {$conta['valor']}");
 }
-
--- funcoes.php
+++ funcoes.php
@@ -1,24 +1,26 @@
 <?php
 
-function quebrarLinha($mensagem){
+function quebrarLinha($mensagem)
+{
     echo "$mensagem" . PHP_EOL;
 }
 
-function sacar(&$conta, $valor){
-    if($conta > $valor){
+function sacar(&$conta, $valor)
+{
+    if ($conta > $valor) {
         $conta -= $valor;
-    } else{
+    } else {
         quebrarLinha("Não possui saldo para sacar!");
     }
     return $conta;
 }
 
-function depositar(&$conta, $valor){
-    if($valor < 0){
+function depositar(&$conta, $valor)
+{
+    if ($valor < 0) {
         quebrarLinha("Não pode efetuar depósito negativo!");
-    }else{
+    } else {
         $conta += $valor;
     }
     return $conta;
-
 }
